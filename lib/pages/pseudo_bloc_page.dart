4d8b16ae75384da11d0a7e4e880498d646acf513
Alejandro Sanchez <alejosandu@gmail.com>
2020-08-11 20:21:16 -0500

    Se separa lógica y render en carpetas diferentes de forma exitosa como patrón pseudo-bloc

--- lib/pages/pseudo_bloc_page.dart
+++ lib/pages/pseudo_bloc_page.dart
@@ -5,5 +5,31 @@ class PseudoBlocPage extends StatefulWidget {
   PseudoBlocPage({Key key}) : super(key: key);
 
   @override
-  PseudoBlocPageState createState() => PseudoBlocPageState();
+  Logic createState() => Logic();
+}
+
+class PseudoBlocPageState extends StatelessWidget {
+  final Logic state;
+
+  PseudoBlocPageState(this.state);
+
+  @override
+  Widget build(BuildContext context) {
+    print("build PseudoBlocPageState");
+    return Scaffold(
+      appBar: AppBar(
+        title: SafeArea(child: Text("Pseudo Bloc")),
+        centerTitle: true,
+      ),
+      body: Container(
+        child: Center(
+          child: Text("Se ha presionado el botón ${state.cont} veces"),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: state.aumentar,
+      ),
+    );
+  }
 }
--- lib/states/pseudo_bloc_state.dart
+++ lib/states/pseudo_bloc_state.dart
@@ -3,39 +3,15 @@ import 'package:flutter/material.dart';
 
 class Logic extends State<PseudoBlocPage> {
   int _cont = 0;
+  int get cont => _cont;
 
-  _aumentar() {
-    print("aumenta");
+  aumentar() {
+    print("aumenta $_cont");
     setState(() => _cont++);
   }
 
-  void setState(fn) {
-    print("setState Logic");
-  }
-
   @override
   Widget build(BuildContext context) {
-    return null;
-  }
-}
-
-class PseudoBlocPageState extends Logic {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: SafeArea(child: Text("Pseudo Bloc")),
-        centerTitle: true,
-      ),
-      body: Container(
-        child: Center(
-          child: Text("Se ha presionado el botón $_cont veces"),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: _aumentar,
-      ),
-    );
+    return PseudoBlocPageState(this);
   }
 }
